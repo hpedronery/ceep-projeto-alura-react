a5ef966d529f3c9fb00d53f1eab41ea3eed7d4a8
hpedronery <hpedronery@gmail.com>
2021-06-09 19:42:31 -0300

    feat(CardNota, FormularioCadastro, ListaDeNotas): Finalização do primeiro curso.

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -3,27 +3,40 @@ import "./estilo.css"
 
 class FormularioCadastro extends Component {
 
-  construtor(){
+  constructor(props){
+    super(props);
     this.titulo = "";
+    this.texto = "";
   }
-  handleMudancaTitulo(evento){
+  _handleMudancaTitulo(evento){
     this.titulo = evento.target.value;
     console.log(this.titulo);
+  }
+  _handleMudancaTexto(evento){
+    this.texto = evento.target.value;
+  }
+  _criarNota(evento){
+    evento.preventDefault();
+    evento.stopPropagation();
+    this.props.criarNota(this.titulo, this.texto);
   }
 
   render() {
     return (
-      <form className="form-cadastro ">
+      <form className="form-cadastro "
+        onSubmit={this._criarNota.bind(this)}
+      >
         <input
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
-          onChange={this.handleMudancaTitulo.bind(this)}
+          onChange={this._handleMudancaTitulo.bind(this)}
         />
         <textarea
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
+          onChange={this._handleMudancaTexto.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
